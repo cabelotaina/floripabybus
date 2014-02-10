4780350aed333bfa4e687f00990f9e210bafc8c0
Maurilio Atila <cabelotaina@gmail.com>
2014-02-10 07:21:04 -0200

    initial commit

---
+++ horarios/scrap_bus_list.R
@@ -0,0 +1,44 @@
+scrap_list <- function(xpath){ 
+	bus_data = NULL;
+	for (i in 1:9 ) { 
+	bus_data[[i]] = xpathSApply(
+				htmlParse( 
+					  paste("linha_det.php_00",
+						i,
+						".html", 
+						sep = ""
+						) , 
+					  encoding = "UTF8" 
+					  ),
+				xpath
+				)
+	}
+	for (i in 10:99 ) { 
+		bus_data[[i]] = xpathSApply(
+					htmlParse( 
+						  paste(
+							"linha_det.php_0",
+							i,
+							".html", 
+							sep = ""
+							) , 
+						  encoding = "UTF8" 
+						  ),
+					xpath
+					)
+	}
+	for (i in 100:209){
+		bus_data[[i]] = 
+		xpathSApply(
+			    htmlParse( 
+				      paste(
+					    "linha_det.php_",
+					    i,
+					    ".html", 
+					    sep = "") , 
+				      encoding = 
+				      "UTF8" ),
+			    xpath)
+	}
+	return(bus_data)
+}
