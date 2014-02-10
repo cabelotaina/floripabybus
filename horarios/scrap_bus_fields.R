7221c784a3324e372c08bccd9480a61fe2e919b8
Maurilio Atila <cabelotaina@gmail.com>
2014-02-10 08:28:54 -0200

    TODO and README in portuguese

--- horarios/scrap_bus_fields.R
+++ horarios/scrap_bus_fields.R
@@ -1,44 +1,22 @@
+file_generator <- function (index){
+	if(index %in% 1:9){
+		paste("linha_det.php_0",index,".html", sep = "")
+	}else if(index %in% 10:99){
+		paste("linha_det.php_00",index,".html", sep = "")
+	}else{
+		paste("linha_det.php_00",index,".html", sep = "")
+	}
+}
+
 scrap_fields <- function(xpath){ 
 	bus_data = NULL;
-	for (i in 1:9 ) { 
-	bus_data[i] = xpathSApply(
-				htmlParse( 
-					  paste("linha_det.php_00",
-						i,
-						".html", 
-						sep = ""
-						) , 
-					  encoding = "UTF8" 
-					  ),
-				xpath
-				)
-	}
-	for (i in 10:99 ) { 
+	for (i in 1:209 ) { 
 		bus_data[i] = xpathSApply(
-					htmlParse( 
-						  paste(
-							"linha_det.php_0",
-							i,
-							".html", 
-							sep = ""
-							) , 
-						  encoding = "UTF8" 
-						  ),
-					xpath
-					)
-	}
-	for (i in 100:209){
-		bus_data[i] = 
-		xpathSApply(
-			    htmlParse( 
-				      paste(
-					    "linha_det.php_",
-					    i,
-					    ".html", 
-					    sep = "") , 
-				      encoding = 
-				      "UTF8" ),
-			    xpath)
+					  htmlParse( file_generator(i)
+						    ,encoding = "UTF8" 
+						    ),
+					  xpath
+					  )
 	}
 	return(bus_data)
 }
