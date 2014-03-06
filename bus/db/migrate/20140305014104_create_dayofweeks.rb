9a3a6f08c11f3add85dc11f6a8a7d804c456e852
Maurilio Atila <cabelotaina@gmail.com>
2014-03-06 18:00:00 -0300

    partial change

--- bus/db/migrate/20140305014104_create_dayofweeks.rb
+++ bus/db/migrate/20140305014104_create_dayofweeks.rb
@@ -2,8 +2,7 @@ class CreateDayofweeks < ActiveRecord::Migration
   def change
     create_table :dayofweeks do |t|
       t.string :day
-      t.string :origin
-      t.string :destiny
+      t.string :path
       t.string :direction
       t.belongs_to :line
       t.timestamps
--- bus/horarios/hpricot_start.rb
+++ bus/horarios/hpricot_start.rb
@@ -1,6 +1,6 @@
 require 'iconv'
 require 'hpricot'
-ic = Iconv.new("UTF-8//IGNORE", "UTF-8")
+ic = Iconv.new("UTF-8", "ISO-8859-1")
 files = Dir["/home/maurilio/code/onibus_floripa/bus/horarios/linhas/*"]
 files.each do |file|
   html = open(file) { |f| Hpricot(ic.iconv(f.read)) }
@@ -59,11 +59,13 @@ files.each do |file|
                        nomes = nomes.gsub("->","=>")
                        nomes = nomes.gsub("-","#")
                        nomes = nomes.split(/#/)
-              	       p nomes[0].squish
-                       p nomes[1].squish
+              	       day = nomes[0].squish
+                       path = nomes[1].squish
+                       direction = ""
                        if !(nomes[2] == nil)
-                        p nomes[2].squish
+                        direction =  nomes[2].squish
                        end
+                       @bus.dayofweeks.new(:day => day, :path => path, :direction => direction).save
 	       end
  i+=2
  end
